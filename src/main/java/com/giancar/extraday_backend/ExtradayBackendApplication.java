0506ed984f1aabcbf140f1580e76a7fd9e4b9170
El_Giancar <giancarmine.cuofano@gmail.com>
2025-03-06 20:15:03 +0100

    Initialize project structure with Gradle and Spring Boot configuration

---
+++ src/main/java/com/giancar/extraday_backend/ExtradayBackendApplication.java
@@ -0,0 +1,13 @@
+package com.giancar.extraday_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtradayBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtradayBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/giancar/extraday_backend/ExtradayBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giancar.extraday_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtradayBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
