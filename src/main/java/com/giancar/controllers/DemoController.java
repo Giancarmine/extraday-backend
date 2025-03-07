7176046d90ee720a5bc8534783ced3425a62a693
El_Giancar <giancarmine.cuofano@gmail.com>
2025-03-07 18:02:55 +0100

    Refactor DemoController to use RestController and return a JSON response

--- src/main/java/com/giancar/controllers/DemoController.java
+++ src/main/java/com/giancar/controllers/DemoController.java
@@ -1,25 +1,31 @@
 package com.giancar.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.giancar.extraday_backend.ExtradayBackendApplication;
 
-@Controller
+@RestController
 public class DemoController {
    private static final Logger logger = LoggerFactory.getLogger(ExtradayBackendApplication.class);
 
-    @RequestMapping(value = "/index.html", method = RequestMethod.GET)
-    @ResponseBody
-    public String index() {
-		logger.info("info log message");
-		logger.warn("warn log message");
-		logger.error("error log message");
-        return "Your first return value";
-    }
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ResponseEntity<Map<String, String>> index() {
+        logger.info("info log message");
+        logger.warn("warn log message");
+        logger.error("error log message");
 
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Your first return value");
+
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
 }
